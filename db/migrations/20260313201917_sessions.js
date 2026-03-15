899790a0708628244c44d572d7157b6025e240b7
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-03-15 16:46:49 +0200

    use sessions.created_at,updated_at,expires_at

--- db/migrations/20260313201917_sessions.js
+++ db/migrations/20260313201917_sessions.js
@@ -6,9 +6,11 @@ exports.up = async function (knex) {
     await knex.schema.createTable('sessions', function (table) {
         table.increments('id');
         table.string('uid', 32).notNullable().unique().collate('utf8mb4_bin');
-        table.integer('user_id').notNullable().references('id').inTable('users').onDelete('RESTRICT');
+        table.integer('user_id').nullable().references('id').inTable('users').onDelete('RESTRICT');
         table.text('custom').notNullable();
-        table.bigInteger('expires').notNullable().index();
+        table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+        table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+        table.timestamp('expires_at').notNullable().index();
     });
 };
 
--- src/helpers/SessionStore.js
+++ src/helpers/SessionStore.js
@@ -1,3 +1,4 @@
+const _try = require('@vbarbarosh/node-helpers/src/_try');
 const db = require('../../db');
 const express_session = require('express-session');
 
@@ -5,19 +6,14 @@ class SessionStore extends express_session.Store
 {
     async get(uid, callback) {
         try {
-            const row = await db('sessions').where({uid}).first();
+            const row = await db('sessions').where({uid}).where('expires_at', '>', db.fn.now()).first();
             console.log('get', uid, '-->', row);
 
             if (!row) {
                 return callback(null, null);
             }
 
-            if (row.expires < Date.now()) {
-                await db('sessions').where({uid}).delete();
-                return callback(null, null);
-            }
-
-            const out = JSON.parse(row.custom);
+            const out = _try(() => JSON.parse(row.custom)) || {};
             out.user_id = row.user_id;
             callback(null, out);
         }
@@ -28,17 +24,17 @@ class SessionStore extends express_session.Store
 
     async set(uid, data, callback) {
         try {
-            const expires = data.cookie?.expires
-                ? new Date(data.cookie.expires).getTime()
-                : Date.now() + (data.cookie?.maxAge || 86400000);
+            const expires_at = data.cookie?.expires
+                ? new Date(data.cookie.expires)
+                : new Date(Date.now() + (data.cookie?.maxAge || 86400000));
 
             console.log('set', uid, '-->', data);
 
             const {user_id, ...custom} = data;
             await db('sessions')
-                .insert({uid, user_id, custom: JSON.stringify(custom), expires})
+                .insert({uid, user_id, custom: JSON.stringify(custom), created_at: db.fn.now(), updated_at: db.fn.now(), expires_at})
                 .onConflict('uid')
-                .merge();
+                .merge(['user_id', 'custom', 'updated_at', 'expires_at']);
 
             callback(null);
         }
@@ -61,8 +57,10 @@ class SessionStore extends express_session.Store
     async touch(uid, data, callback) {
         try {
             console.log('touch', uid, '-->', data);
-            const expires = data.cookie?.expires ? new Date(data.cookie.expires).getTime() : Date.now() + 86400000;
-            await db('sessions').where({uid}).update({expires});
+            const expires_at = data.cookie?.expires
+                ? new Date(data.cookie.expires)
+                : new Date(Date.now() + (data.cookie?.maxAge || 86400000));
+            await db('sessions').where({uid}).update({expires_at, updated_at: db.fn.now()});
             callback(null);
         }
         catch (error) {
