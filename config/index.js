370aaae7fdc252ee389e4cfa94b8c2f03150cd2b
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-03-21 20:38:08 +0200

    debugging

--- config/index.js
+++ config/index.js
@@ -57,4 +57,6 @@ function secret_hkdf(namespace)
     return Buffer.from(crypto.hkdfSync('sha256', SECRET, 'authwall', namespace, 32)).toString('hex');
 }
 
+console.log(config);
+
 module.exports = config;
