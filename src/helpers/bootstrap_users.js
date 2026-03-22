bb511d545e935d02cdeaa78ba81bca8406834aac
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-03-23 00:11:19 +0200

    bootstrap users with several emails

--- src/helpers/bootstrap_users.js
+++ src/helpers/bootstrap_users.js
@@ -16,32 +16,38 @@ async function bootstrap_users()
     console.log('👤 Seeding users...');
     for (const user_seed of config.seed_users) {
         const {username, email, password_hash, display_name} = user_seed;
-        if (!email && !username) {
+        const emails = Array.isArray(email) ? email : [email].filter(Boolean);
+
+        const username_normalized = normalize_username(username);
+        const emails_normalized = emails.map(normalize_email).filter(v => v);
+        if (!username_normalized && !emails_normalized.length) {
             console.log(`⚠️ Skipping invalid user seed: [${JSON.stringify(user_seed).slice(1, -1)}]`);
             continue;
         }
 
-        const email_normalized = normalize_email(email);
-        if (email_normalized) {
-            const ident = await db('user_identities').where({type: const_user_identity.email, value_normalized: email_normalized}).first();
-            if (ident) {
-                console.log(`ℹ️ Skipping existing user username=[${username}] email=[${email}]`);
-                continue;
-            }
-        }
+        // Either find user or create one
+        const ident = await db('user_identities')
+            .where(function (q) {
+                if (username_normalized) {
+                    q.orWhere({type: const_user_identity.username, value_normalized: username_normalized});
+                }
+                if (emails_normalized.length) {
+                    q.orWhere(function (qq) {
+                        qq.where('type', const_user_identity.email);
+                        qq.whereIn('value_normalized', emails_normalized);
+                    });
+                }
+            })
+            .first();
 
-        const username_normalized = normalize_username(username);
-        if (username_normalized) {
-            const ident = await db('user_identities').where({type: const_user_identity.username, value_normalized: username_normalized}).first();
-            if (ident) {
-                console.log(`ℹ️ Skipping existing user username=[${username}] email=[${email}]`);
-                continue;
-            }
+        let user_id;
+        let user_created = !ident;
+        if (ident) {
+            user_id = ident.user_id;
         }
-
-        const now = new Date();
-        await db.transaction(async function (trx) {
-            const [user_id] = await trx('users').insert({
+        else {
+            const now = new Date();
+            [user_id] = await db('users').insert({
                 uid: random_uid_user(),
                 slug: random_slug(),
                 password_hash,
@@ -49,31 +55,29 @@ async function bootstrap_users()
                 created_at: now,
                 updated_at: now,
             });
-            if (email_normalized) {
-                await trx('user_identities').insert({
-                    user_id,
-                    type: const_user_identity.email,
-                    value: email,
-                    value_normalized: email_normalized,
-                    created_at: now,
-                    updated_at: now,
-                    verified_at: now,
-                });
-            }
-            if (username_normalized) {
-                await trx('user_identities').insert({
-                    user_id,
-                    type: const_user_identity.username,
-                    value: username,
-                    value_normalized: username_normalized,
-                    created_at: now,
-                    updated_at: now,
-                    verified_at: now,
-                });
-            }
-        });
+        }
 
-        console.log(`👤 Added user username=[${username}] email=[${email}]`);
+        const idents1 = await db('user_identities').where({user_id}).count();
+
+        const now = new Date();
+        const base = {user_id, created_at: now, updated_at: now, verified_at: now};
+        const rows = [
+            {...base, type: const_user_identity.username, value: username, value_normalized: username_normalized},
+            ...emails.map(v => ({...base, user_id, type: const_user_identity.email, value: v, value_normalized: normalize_email(v)})),
+        ].filter(v => v.value_normalized);
+        await db('user_identities').insert(rows).onConflict(['type', 'value_normalized']).ignore();
+
+        const idents2 = await db('user_identities').where({user_id}).count();
+
+        if (user_created) {
+            console.log(`👤 Created user username=[${username}] email=[${email}]`);
+        }
+        else if (idents1 < idents2) {
+            console.log(`➕ Added identities username=[${username}] email=[${email}]`);
+        }
+        else {
+            console.log(`⏭️ No changes username=[${username}] email=[${email}]`);
+        }
     }
 
     console.log('✅ Users seeding complete');
