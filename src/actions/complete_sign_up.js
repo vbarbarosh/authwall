2deee3d16d24492133c636e41ba3e64342e7a620
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-03-28 20:12:29 +0200

    sign up with email

--- src/actions/complete_sign_up.js
+++ src/actions/complete_sign_up.js
@@ -1,10 +1,23 @@
+const config = require('../../config');
+const fs_path_resolve = require('@vbarbarosh/node-helpers/src/fs_path_resolve');
 const redirect = require('../helpers/redirect');
 const replace_session = require('../helpers/replace_session');
+const send_email = require('../helpers/send_email');
 
 async function complete_sign_up(req, res, user)
 {
     await replace_session(req, user);
+
     redirect(req, res);
+
+    await send_email({
+        user,
+        path: fs_path_resolve(__dirname, '../../design/emails/welcome.txt'),
+        placeholders: {
+            display_name: user.display_name,
+            sign_in_link: config.public_url + config.pages.sign_in,
+        },
+    });
 }
 
 module.exports = complete_sign_up;
