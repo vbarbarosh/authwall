bc395098f599850881013761598198bd029affac
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-03-29 13:38:20 +0300

    redirect: stricter check for return url

--- src/helpers/redirect.js
+++ src/helpers/redirect.js
@@ -1,11 +1,39 @@
+const config = require('../../config');
+const is_relative_url = require('./is_relative_url');
+
 function redirect(req, res, default_url = '/')
 {
-    if (typeof req.query.return === 'string' && req.query.return.startsWith('/')) {
-        res.redirect(req.query.return);
+    res.redirect(extract_return_url(req) ?? default_url);
+}
+
+function extract_return_url(req)
+{
+    if (typeof req.query.return !== 'string') {
+        return null;
     }
-    else {
-        res.redirect(default_url);
+
+    // accept relative urls only
+    if (!is_relative_url(req.query.return)) {
+        return null;
     }
+
+    try {
+        const base = new URL(config.public_url);
+        const url = new URL(req.query.return, base);
+        if (url.origin === base.origin) {
+            switch (url.pathname) {
+            case config.pages.sign_in:
+            case config.pages.sign_out:
+                break;
+            default:
+                return url.pathname + url.search + url.hash;
+            }
+        }
+    }
+    catch {
+    }
+
+    return null;
 }
 
 module.exports = redirect;
