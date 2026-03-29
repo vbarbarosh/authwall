aab909d28c9c9261a0475a00e859d7012e3adfb0
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-03-29 12:13:46 +0300

    send_email: throw if failed

--- src/helpers/send_email.js
+++ src/helpers/send_email.js
@@ -12,8 +12,7 @@ async function send_email(params)
 
     const email_and_name = to ?? await get_user_email_and_name(user_id ?? user.id);
     if (!email_and_name) {
-        console.log(`User user_id=${user_id ?? user.id} has no associated email`);
-        return;
+        throw new Error(`User user_id=${user_id ?? user.id} has no associated email`);
     }
 
     client ??= new resend.Resend(config.resend_key);
@@ -30,6 +29,31 @@ async function send_email(params)
 
     console.log(`[send_email_request] ${JSON.stringify(request)}`);
     console.log(`[send_email_response] ${JSON.stringify(response)}`);
+
+    // {
+    //     "data": null,
+    //     "error": {
+    //         "statusCode": 422,
+    //         "name": "validation_error",
+    //         "message": "Invalid `to` field. The email address needs to follow the `email@example.com` or `Name <email@example.com>` format."
+    //     },
+    //     "headers": {
+    //         "cf-cache-status": "DYNAMIC",
+    //         "cf-ray": "9e3a56a85adf186a-KIV",
+    //         "connection": "keep-alive",
+    //         "content-length": "172",
+    //         "content-type": "application/json",
+    //         "date": "Sat, 28 Mar 2026 23:15:44 GMT",
+    //         "ratelimit-limit": "5",
+    //         "ratelimit-policy": "5;w=1",
+    //         "ratelimit-remaining": "4",
+    //         "ratelimit-reset": "1",
+    //         "server": "cloudflare"
+    //     }
+    // }
+    if (response.error) {
+        throw new Error(response.error.message);
+    }
 }
 
 module.exports = send_email;
