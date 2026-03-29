2e416e1ca8c97ca9761671237498cfddfedc1c8a
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-03-30 02:10:09 +0300

    extract create_app

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 #!/usr/bin/env node
 
-const SessionStore = require('./helpers/SessionStore');
 const bootstrap_database = require('./helpers/bootstrap_database');
 const bootstrap_users = require('./helpers/bootstrap_users');
 const cli = require('@vbarbarosh/node-helpers/src/cli');
 const config = require('../config');
-const express = require('express');
-const express_log = require('@vbarbarosh/express-helpers/src/express_log');
-const express_routes = require('./helpers/express/express_routes');
+const create_app = require('./create_app');
 const express_run = require('@vbarbarosh/express-helpers/src/express_run');
-const express_session = require('express-session');
-const fs_exists = require('@vbarbarosh/node-helpers/src/fs_exists');
-const fs_path_resolve = require('@vbarbarosh/node-helpers/src/fs_path_resolve');
-const http_proxy_middleware = require('http-proxy-middleware');
-const random_base62 = require('./helpers/random/random_base62');
-const random_uid_session = require('./helpers/random/random_uid_session');
-const urlmod = require('@vbarbarosh/node-helpers/src/urlmod');
 
 cli(main);
 
@@ -24,163 +14,7 @@ async function main()
     await bootstrap_database();
     await bootstrap_users();
 
-    const app = express();
-
-    app.set('trust proxy', true);
-
-    app.use(express_log({file: config.log_file_http}));
-
-    app.use('/auth/uploads', express.static(fs_path_resolve(__dirname, '../data/uploads')));
-
-    app.use('/auth', express.json());
-    app.use('/auth', express.urlencoded({extended: false}));
-
-    app.use(express_session({
-        genid: random_uid_session,
-        store: new SessionStore(),
-        resave: false,
-        saveUninitialized: false,
-        secret: config.secrets.express_session,
-        cookie: {
-            path: '/',
-            httpOnly: true,
-            sameSite: 'lax',
-            secure: config.public_url.startsWith('https://'),
-            maxAge: 30 * 24 * 60 * 60 * 1000,
-        },
-    }));
-    app.use('/auth', function (req, res, next) {
-        if (req.session && !req.session.ip) {
-            req.session.ip = req.ip;
-            req.session.ua = req.headers['user-agent'] ?? 'n/a';
-        }
-        if (req.session && !req.session.csrf_token) {
-            req.session.csrf_token = random_base62();
-        }
-        next();
-    });
-
-    app.get('/auth', function (req, res) {
-        if (req.session?.user_id) {
-            res.redirect(config.pages.profile);
-        }
-        else {
-            res.redirect(config.pages.sign_in);
-        }
-    });
-
-    express_routes(app, require('./routes/dev'));
-    express_routes(app, require('./routes/email_verify'));
-    express_routes(app, require('./routes/magic_link'));
-    express_routes(app, require('./routes/oauth_google'));
-    express_routes(app, require('./routes/password'));
-    express_routes(app, require('./routes/profile'));
-    express_routes(app, require('./routes/sessions'));
-    express_routes(app, require('./routes/status'));
-
-    // Support for mountable design
-    app.use('/auth/', express.static(fs_path_resolve(__dirname, '../design/public_html'), {extensions: ['html']}));
-
-    for (const key in config.pages) {
-        const path = config.pages[key];
-        app.get(path, async function (req, res) {
-            const spa = fs_path_resolve(__dirname, '../design/public_html/spa.html');
-            if (await fs_exists(spa)) {
-                res.sendFile(spa);
-            }
-            else {
-                res.type('text').send(`Empty page\n\n${key}: GET ${path}`);
-            }
-        });
-    }
-
-    app.use(clean_headers);
-    app.use(sign_in_required);
-    app.use(http_proxy_middleware.createProxyMiddleware({
-        target: config.target_url,
-        changeOrigin: true,
-        pathFilter: function (pathname) {
-            return !(pathname === '/auth' || pathname.startsWith('/auth/'));
-        },
-        on: {
-            proxyReq: function (proxy_req, req) {
-                // // ⚠️ Beware that headers are proxied too
-                // // ⚠️ Take special care not to special headers
-                // // curl http://localhost:3000/foo/bar -H X-Auth-User:foo -H 'Cookie: connect.sid=s%3A7lIgELaKCxNmyCw5iDcFsAUq6-nVQ6o6.LCvDq0niOJtMT75hMUL2sqvssyXC4Ilm99ftI9Fa4BE'
-                // // curl http://localhost:3000/bypass/foo/bar -H X-Auth-User:foo
-                // const headers = proxy_req.getHeaderNames();
-                // for (let i = 0, ii = headers.length; i < ii; ++i) {
-                //     const header = headers[i];
-                //     if (header.startsWith('x-auth-')) {
-                //         proxy_req.removeHeader(header);
-                //     }
-                // }
-                if (req.session.user_uid) {
-                    proxy_req.setHeader('X-Auth-User',  req.session.user_uid);
-                }
-            },
-        },
-    }));
-
-    app.use(error_handler);
-
-    function error_handler(error, req, res, next)
-    {
-        console.log('⚠️ error_handler', req.url, req.originalUrl);
-        console.error(error);
-
-        if (req.session) {
-            req.session.error = error.message;
-        }
-
-        // Prevent infinite redirects
-        if (req.url === req.originalUrl) {
-            res.redirect('/auth/sign-in');
-        }
-        // if (req.originalUrl.startsWith('/auth/google')) {
-        //     res.redirect('/auth/sign-in');
-        // }
-        else {
-            res.redirect(req.originalUrl);
-        }
-    }
+    const app = await create_app();
 
     await express_run(app, config.port, config.listen);
-}
-
-function sign_in_required(req, res, next)
-{
-    if (config.public_paths.includes(req.path)) {
-        next();
-        return;
-    }
-
-    if (req.path.startsWith('/bypass/')) {
-        next();
-        return;
-    }
-
-    if (!req.session.user_id) {
-        console.log('auth_go_to_login', req.method, req.path);
-        return res.redirect(urlmod('/auth/sign-in', {return: req.originalUrl}));
-    }
-    console.log('auth_next', req.method, req.path);
-    next();
-}
-
-function clean_headers(req, res, next)
-{
-    // https://github.com/chimurai/http-proxy-middleware/issues/472?utm_source=chatgpt.com
-    // ⚠️ Beware that headers are proxied too
-    // ⚠️ Take special care not to special headers
-    // curl http://localhost:3000/foo/bar -H X-Auth-User:foo -H 'Cookie: connect.sid=s%3A7lIgELaKCxNmyCw5iDcFsAUq6-nVQ6o6.LCvDq0niOJtMT75hMUL2sqvssyXC4Ilm99ftI9Fa4BE'
-    // curl http://localhost:3000/bypass/foo/bar -H X-Auth-User:foo
-    const keys = Object.keys(req.headers);
-    for (let i = 0, ii = keys.length; i < ii; ++i) {
-        const s = keys[i];
-        if (s.startsWith('x-auth-')) {
-            delete req.headers[s];
-        }
-    }
-    next();
 }
