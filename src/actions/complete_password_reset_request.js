e8996176ed609b97e00c77c878fee179d18b3915
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-03-29 13:47:50 +0300

    complete_password_reset_request required email to be sent

--- src/actions/complete_password_reset_request.js
+++ src/actions/complete_password_reset_request.js
@@ -2,15 +2,13 @@ const config = require('../../config');
 const db = require('../../db');
 const fs_path_resolve = require('@vbarbarosh/node-helpers/src/fs_path_resolve');
 const redirect = require('../helpers/redirect');
-const send_email_nothrow = require('../helpers/send_email_nothrow');
+const send_email = require('../helpers/send_email');
 const urlmod = require('@vbarbarosh/node-helpers/src/urlmod');
 
 async function complete_password_reset_request(req, res, user_id, email, token)
 {
-    redirect(req, res, config.pages.password_reset_notice);
-
     const user = await db('users').where('id', user_id).first();
-    await send_email_nothrow({
+    await send_email({
         to: {name: user.display_name, email},
         path: fs_path_resolve(__dirname, '../../design/emails/password-reset.txt'),
         placeholders: {
@@ -18,6 +16,8 @@ async function complete_password_reset_request(req, res, user_id, email, token)
             link: config.public_url + urlmod(config.pages.password_reset_confirm, {token}),
         },
     });
+
+    redirect(req, res, config.pages.password_reset_notice);
 }
 
 module.exports = complete_password_reset_request;
