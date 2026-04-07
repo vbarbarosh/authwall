8d733d0ae01252866a6bf918573d4edae8cfbedc
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-04-07 22:18:03 +0300

    redirect: support for absolute urls

--- src/helpers/redirect.js
+++ src/helpers/redirect.js
@@ -1,39 +1,39 @@
-const config = require('../../config');
-const is_relative_url = require('./is_relative_url');
-
 function redirect(req, res, default_url = '/')
 {
     res.redirect(extract_return_url(req) ?? default_url);
 }
 
+// ⚠️ Check url against valid redirect urls; config.redirect_domain could be introduced for that
 function extract_return_url(req)
 {
     if (typeof req.query.return !== 'string') {
         return null;
     }
 
-    // accept relative urls only
-    if (!is_relative_url(req.query.return)) {
-        return null;
-    }
+    return req.query.return;
 
-    try {
-        const base = new URL(config.public_url);
-        const url = new URL(req.query.return, base);
-        if (url.origin === base.origin) {
-            switch (url.pathname) {
-            case config.pages.sign_in:
-            case config.pages.sign_out:
-                break;
-            default:
-                return url.pathname + url.search + url.hash;
-            }
-        }
-    }
-    catch {
-    }
-
-    return null;
+    // // accept relative urls only
+    // if (!is_relative_url(req.query.return)) {
+    //     return null;
+    // }
+    //
+    // try {
+    //     const base = new URL(config.public_url);
+    //     const url = new URL(req.query.return, base);
+    //     if (url.origin === base.origin) {
+    //         switch (url.pathname) {
+    //         case config.pages.sign_in:
+    //         case config.pages.sign_out:
+    //             break;
+    //         default:
+    //             return url.pathname + url.search + url.hash;
+    //         }
+    //     }
+    // }
+    // catch {
+    // }
+    //
+    // return null;
 }
 
 module.exports = redirect;
