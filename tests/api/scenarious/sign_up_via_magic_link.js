87c2212e7861598a5061bf59711c657482171015
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-04-10 18:25:41 +0300

    clean

--- tests/api/scenarious/sign_up_via_magic_link.js
+++ tests/api/scenarious/sign_up_via_magic_link.js
@@ -9,7 +9,7 @@ describe('sign up via magic link | scenarios', function () {
         await this.client.post_json('/auth/magic-link/request', {email, _csrf: status.csrf_token});
 
         const [, code] = this.sent_emails[0].text.match(/confirmation page:\s*(\d+)/i);
-        const r = await this.client.post_json('/auth/magic-link/confirm', {email, code, _csrf: status.csrf_token});
+        await this.client.post_json('/auth/magic-link/confirm', {email, code, _csrf: status.csrf_token});
 
         const status2 = await this.client.get_json('/auth/status');
         assert.strictEqual(status2.error, null);
