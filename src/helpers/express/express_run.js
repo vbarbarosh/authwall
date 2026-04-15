6ec4162df0dee031744bc9f132004e4799cd8123
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-04-15 23:23:32 +0300

    using als.logger in express_run

---
+++ src/helpers/express/express_run.js
@@ -0,0 +1,37 @@
+const Promise = require('bluebird');
+const als = require('../als');
+
+function express_run(app, port = 3000, host = 'localhost')
+{
+    const server = app.listen(port, host, function () {
+        const {address, port} = this.address();
+        als.logger.write(`[express_run] Listening to ${address}:${port}`);
+    });
+
+    // https://joseoncode.com/2014/07/21/graceful-shutdown-in-node-dot-js/
+    // process.on('SIGTERM', sigterm);
+    // process.on('SIGINT', sigint);
+    //
+    // server.on('close', function () {
+    //     console.log(`[express_run] end`);
+    //     process.off('SIGTERM', sigterm);
+    //     process.off('SIGINT', sigint);
+    // });
+
+    // return pending(cb => server.once('close', cb));
+    return new Promise(function (resolve) {
+        server.once('close', resolve);
+    });
+
+    function sigterm() {
+        als.logger.write('[express_run] SIGTERM');
+        server.close();
+    }
+
+    function sigint() {
+        als.logger.write('[express_run] SIGINT');
+        server.close();
+    }
+}
+
+module.exports = express_run;
