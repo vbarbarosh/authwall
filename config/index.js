34673cf31ab9d49b5e170a01002e221cff73b15f
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-04-17 23:16:18 +0300

    fix config/index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ const yaml = require('yaml');
 
 const AUTHWALL_MYSQL = process.env.AUTHWALL_MYSQL;
 
-const knexvars = AUTHWALL_MYSQL ? knexfile.mysql : knexfile.sqlite;
+const knexvars = AUTHWALL_MYSQL ? {...knexfile.mysql, connection: AUTHWALL_MYSQL} : knexfile.sqlite;
 
 const logs_dir = fs_path_resolve(__dirname, '../data/logs');
 const uploads_dir = fs_path_resolve(__dirname, '../data/uploads');
