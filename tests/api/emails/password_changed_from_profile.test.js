1695f86dd6607a941fda1e777f67c275682cb801
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-04-18 12:24:30 +0300

    fix race condition

--- tests/api/emails/password_changed_from_profile.test.js
+++ tests/api/emails/password_changed_from_profile.test.js
@@ -14,6 +14,7 @@ describe('emails • password_changed_via_reset_link', function () {
             password: 'pass456',
             password_confirm: 'pass456',
         });
+        await this.wait_for_emails(2);
 
         const actual = this.sent_emails.map(v => v.name);
         const expected = [const_email.new_sign_in, const_email.password_changed_from_profile];
