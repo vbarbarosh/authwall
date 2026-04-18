11cd98f16a6ed7eb510fd9188f73cecd5d7277b5
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-04-18 12:20:31 +0300

    clean

--- tests/api/emails/confirm_email.test.js
+++ tests/api/emails/confirm_email.test.js
@@ -4,15 +4,12 @@ const const_email = require('../../../src/helpers/const/const_email');
 describe('emails • confirm_email', function () {
 
     it('should be sent after requesting email confirmation', async function () {
-        await this.add_user({email: 'mocha@authwall.test', password: 'pass123', verified: false});
-
-        await this.http_post_json('/auth/sign-in', {username: 'mocha@authwall.test', password: 'pass123'});
-        await this.wait_for_emails(1);
+        await this.sign_in({email: 'mocha@authwall.test', password: 'pass123', verified: false});
 
         await this.http_post_json('/auth/email-verify/request');
 
         const actual = this.sent_emails.map(v => v.name);
-        const expected = [const_email.new_sign_in, const_email.confirm_email];
+        const expected = [const_email.confirm_email];
         assert.deepStrictEqual(actual, expected);
     });
 
