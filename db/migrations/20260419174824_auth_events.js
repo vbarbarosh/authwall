cbc634650b2b78445f63895f77b2fd45850e4e3d
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2026-04-19 21:47:17 +0300

    update db/migrations/20260419174824_auth_events.js

--- db/migrations/20260419174824_auth_events.js
+++ db/migrations/20260419174824_auth_events.js
@@ -7,19 +7,32 @@ const utf8mb4_bin = require('../utf8mb4_bin');
 exports.up = async function (knex) {
     await knex.schema.createTable('auth_events', function (table) {
         table.increments('id');
-        utf8mb4_bin(table.string('uid', 32).notNullable().unique());
-        table.integer('user_id').unsigned().nullable().index().references('id').inTable('users').onDelete('RESTRICT');
-        utf8mb4_bin(table.string('session_uid', 32).nullable().index());
-        utf8mb4_bin(table.string('event_type', 64).notNullable().index());
-        utf8mb4_bin(table.string('event_status', 32).notNullable().index());
-        utf8mb4_bin(table.string('identity_type', 32).nullable().index());
+        utf8mb4_bin(table.string('uid', 32).notNullable());
+        table.integer('user_id').unsigned().nullable();
+        utf8mb4_bin(table.string('session_uid', 32).nullable());
+        utf8mb4_bin(table.string('event_type', 64).notNullable());
+        utf8mb4_bin(table.string('event_status', 32).notNullable());
+        utf8mb4_bin(table.string('identity_type', 32).nullable());
         table.string('identity_value', 255).nullable();
         utf8mb4_bin(table.string('identity_value_normalized', 255).nullable());
         table.string('ip', 64).nullable();
-        table.string('ua', 512).nullable().comment('User Agent');
+        table.string('ua', 512).nullable();
         table.text('custom').notNullable();
-        table.timestamp('created_at').notNullable().index();
+        table.timestamp('created_at').notNullable();
 
+        // constraints
+        table.unique(['uid']);
+        table.foreign('user_id').references('id').inTable('users').onDelete('RESTRICT');
+
+        // indexes
+        table.index(['user_id']);
+        table.index(['session_uid']);
+        table.index(['event_type']);
+        table.index(['event_status']);
+        table.index(['identity_type']);
+        table.index(['created_at']);
+
+        // composite indexes
         table.index(['user_id', 'created_at']);
         table.index(['event_type', 'created_at']);
         table.index(['identity_type', 'identity_value_normalized']);
